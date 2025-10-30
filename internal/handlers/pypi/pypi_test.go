991f184b8bee039e31bb31fb2b533be83ad8a7b1
Benjamin Schubert <contact@benschubert.me>
2025-10-30 21:17:15 +0100

    Add benchmark for the modification of the Pypi data

--- internal/handlers/pypi/pypi_test.go
+++ internal/handlers/pypi/pypi_test.go
@@ -1,14 +1,20 @@
-package pypi_test
+package pypi
 
 import (
+	"bytes"
+	"encoding/base64"
+	"io"
 	"net/http"
 	"net/url"
 	"testing"
 
-	"github.com/benjaminschubert/locaccel/internal/handlers/pypi"
+	"github.com/stretchr/testify/require"
+
 	"github.com/benjaminschubert/locaccel/internal/handlers/testutils"
 	"github.com/benjaminschubert/locaccel/internal/httpclient"
 )
+
+var pytestInfo []byte = nil
 
 func TestInstallPythonPackages(t *testing.T) {
 	t.Parallel()
@@ -17,7 +23,7 @@ func TestInstallPythonPackages(t *testing.T) {
 		t,
 		"pypi",
 		func(handler *http.ServeMux, client *httpclient.Client, upstreamCaches []*url.URL) {
-			pypi.RegisterHandler(
+			RegisterHandler(
 				"https://pypi.org",
 				"https://files.pythonhosted.org",
 				handler,
@@ -46,4 +52,33 @@ func TestInstallPythonPackages(t *testing.T) {
 		},
 		true,
 	)
+}
+
+func BenchmarkJSONRewrite(b *testing.B) {
+	if pytestInfo == nil {
+		req, err := http.NewRequestWithContext(
+			b.Context(),
+			"GET",
+			"https://pypi.org/simple/pytest/",
+			nil,
+		)
+		require.NoError(b, err)
+		req.Header.Add("Accept", "application/vnd.pypi.simple.v1+json")
+
+		resp, err := http.DefaultClient.Do(req)
+		require.NoError(b, err)
+
+		pytestInfo, err = io.ReadAll(resp.Body)
+		require.NoError(b, err)
+		require.NoError(b, resp.Body.Close())
+	}
+
+	cdn := "https://files.pythonhosted.org"
+	encodedCDN := "/cnd/" + base64.StdEncoding.EncodeToString([]byte(cdn))
+
+	for b.Loop() {
+		d := bytes.Clone(pytestInfo)
+		_, err := rewriteJsonV1(d, cdn, encodedCDN)
+		require.NoError(b, err)
+	}
 }
